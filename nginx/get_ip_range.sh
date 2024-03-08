1eff5ce4153a4db588ba902b9b574dddf1be3d55
Tomer <tomer@tomer.com>
2024-03-08 11:43:18 +0200

    Fixed typos in ip-pool

--- nginx/get_ip_range.sh
+++ nginx/get_ip_range.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip_base=$(docker network inspect -f '{{.IPAM.Config}}' kind | awk '{print $3}' | tr -d '{/' | awk -F'.' '{printf "%s.%s.%s\n", $1, $2, $3}')
+ip_base=$(docker network inspect kind --format '{{ (index .IPAM.Config 0).Subnet }}' | awk -F'.' '{printf "%s.%s.%s", $1, $2, $3}')
 
 echo "$ip_base.120-$ip_base.129"
 
